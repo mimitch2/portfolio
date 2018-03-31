c7773bdbf8cb996f7f50d9a819429b004be5b4b0
mimitch2 <mike@melodix.com>
2018-03-31 16:21:22 -0500

    got the robot section done and lined up, probably should break it all out to a descreet component

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -12,7 +12,16 @@ import RaisedButton from 'material-ui/RaisedButton';
 // import Fade from 'react-reveal/Fade';
 import './App.css';
 
-const languages = ['ar', 'az', 'be', 'bg', 'bn', 'bs', 'cs', 'da', 'de', 'dz', 'el', 'en', 'en-gb', 'en-us', 'es', 'et', 'fa', 'fi', 'fil', 'fr', 'he', 'hi', 'hr', 'hu', 'hy', 'id', 'is', 'it', 'ka', 'kk', 'km', 'ko', 'lo', 'lt', 'lv', 'mk', 'mn', 'ms', 'my', 'ne', 'no', 'ro', 'ru', 'sk', 'sl', 'sq', 'sr', 'sv', 'sw', 'th', 'tk', 'uk', 'vi', 'zh']
+const languages = ['ar', 'az', 'bg', 'bn', 'bs', 'cs', 'da', 'de', 'dz', 'el', 'en', 'en-gb', 'en-us', 'es', 'et', 'fa', 'fi', 'fil', 'fr', 'he', 'hi', 'hr', 'hu', 'hy', 'id', 'it', 'ka', 'kk', 'km', 'ko', 'lo', 'lt', 'lv', 'mk', 'mn', 'ms', 'my', 'ne', 'no', 'ro', 'ru', 'sk', 'sl', 'sq', 'sr', 'sv', 'sw', 'th', 'tk', 'uk', 'vi', 'zh']
+
+const style = {
+  hide: {
+    display: 'none'
+  },
+  show: {
+    display: 'inline'
+  }
+}
 
 
 class App extends Component {
@@ -23,7 +32,8 @@ class App extends Component {
       userInput: '',
       returnedRobot: '',
       robotName: '',
-      greeting: ''
+      greeting: '',
+      cssStyle : style.hide
     };
   }
 
@@ -36,13 +46,18 @@ getInput=(e)=>{
 
   getNewRobot=()=>{
 
-    const languageCode = languages[Math.floor((Math.random() * languages.length-1) + 1)]
-    console.log(languageCode);
     this.setState({
-      returnedRobot: `https://robohash.org/${this.state.userInput}.png?set=set3`,
+      returnedRobot: `https://robohash.org/${this.state.userInput}.png`,
       robotName: this.state.userInput,
       userInput: ''
     });
+    this.fetchGreeting()
+  }
+
+  fetchGreeting=()=>{
+    const languageCode = languages[Math.floor((Math.random() * languages.length-1))]
+    console.log(languageCode);
+
     fetch(`https://fourtonfish.com/hellosalut/?lang=${languageCode}`)
       .then(res => res.json())
       .then(
@@ -50,7 +65,8 @@ getInput=(e)=>{
           // this.getNewWord(word)
           this.setState({
             isLoaded: true,
-            greeting: this.getNewWord(word)
+            greeting: this.getNewWord(word),
+            cssStyle: style.show
           });
         },
         (error) => {
@@ -61,15 +77,13 @@ getInput=(e)=>{
 
         }
       )
-
   }
 
 
-  getNewWord(input){
+  getNewWord=(input)=>{//this parses and translates the character codes for non-arabic languages
     if(!input.hello.search("&")){
-      const newWord = input.hello.replace(/&/g, "").replace(/#/g, "").replace(/;/g, ",")
-      const newArr = newWord.split(',').map(Number)
-      const translatedWord = String.fromCharCode(...newArr);
+      const newWord = input.hello.replace(/&/g, "").replace(/#/g, "").replace(/;/g, ",").split(',').map(Number)
+      const translatedWord = String.fromCharCode(...newWord);
       console.log(translatedWord, typeof(translatedWord));
       return translatedWord
     }else{
@@ -78,7 +92,7 @@ getInput=(e)=>{
 
   }
 
-  buttonState(){
+  buttonState=()=>{
     if(!this.state.userInput){
       return true
     }else{
@@ -115,26 +129,25 @@ Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor 
             <div className="js-comp-image-div">
               <img src= {jsCompImage} className = 'js-comp-image' alt="computer with javascript" />
             </div>
+            <div className = 'robot-wrapper'>
+              <div className="robot">
 
-            <div className="robot">
+                <img src={this.state.returnedRobot} style= {{width: '300px', heigth: '300px'}} alt="" />
+                <h2 className = "greeting-text" style={this.state.cssStyle}>{this.state.greeting} fren. I am {this.state.robotName}.</h2>
 
-              <img src={this.state.returnedRobot} style= {{width: '300px', heigth: '300px'}} alt="" />
-              <h2>{this.state.greeting} fren. I am {this.state.robotName}</h2>
+                <form onSubmit={(e)=>{if (this.state.userInput.length > 0)this.getNewRobot();
+                  e.preventDefault()}}> <TextField
+                    style={{fontSize: '18px'}}
+                    type="text"
+                    hintText="Type any name"
+                    value={this.state.userInput}
+                    onChange={this.getInput} underlineFocusStyle={{borderBottom: "2px solid rgba(84, 115, 115, .8)"}} fullWidth={false}/></form>
 
-              <form onSubmit={(e)=>{if (this.state.userInput.length > 0)this.getNewRobot();
-                e.preventDefault()}}> <TextField
-                  style={{fontSize: '18px'}}
-                  type="text"
-                  hintText="Type any name!"
-                  value={this.state.userInput}
-                  onChange={this.getInput} underlineFocusStyle={{borderBottom: "2px solid rgba(84, 115, 115, .8)"}} fullWidth={false}/></form>
+                <RaisedButton label="Get robot"  onClick = {this.getNewRobot} style={{fontSize:'18px', width: "256px"}} disabled={this.buttonState()}/>
 
-              <RaisedButton label="Get robot"  onClick = {this.getNewRobot} style={{fontSize:'18px', width: "256px"}} disabled={this.buttonState()}/>
-              <h3><i className="fa fa-arrow-up
-"></i>&nbsp;Do you want robots?.</h3>
+              </div>
+              <h3 className="robot-announce"><i className="fa fa-arrow-left"></i>&nbsp;&nbsp;Do you want robots? Because this is how you get robots.</h3>
             </div>
-
-
             <div id = "bottom-space"></div>
           </Zoom>
           <footer className="footer">
